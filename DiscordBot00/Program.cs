a4cdbca9ae79cbecf3ca4887c6e4ad6ece168a5b
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-07-25 17:43:50 +0700

    add basic template command

--- DiscordBot00/Program.cs
+++ DiscordBot00/Program.cs
@@ -17,7 +17,43 @@ namespace DiscordBot00
         private DiscordClient _client;
         public void Start()
         {
-            _client = new DiscordClient();
+            _client = new DiscordClient(x=>{
+                x.AppName = "Discord Bot 00";
+                x.AppUrl = "";
+                x.AppVersion = "0.01";
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            _client.UsingCommands(x=>{
+                x.PrefixChar = '~';
+                x.AllowMentionPrefix = true;
+            });
+
+            var token = "MjA0ODUyOTk1ODkxMDAzMzky.CndzAA.n4TL112_e4-ej8LnOrnMOJ-R6s4";
+
+            CreateCommands();
+
+            _client.ExecuteAndWait(async () => {
+                await _client.Connect(token);
+            });
+        }
+
+        public void CreateCommands()
+        {
+            var commandService = _client.GetService<CommandService>();
+            commandService.CreateCommand("tosmap")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("This command is not available right now because the dev is a shithole. Please commit sudoku, dev!!");
+                });
+        }
+
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            //[INFO][DISCORD]Client connected
+            Console.WriteLine($"[{e.Severity}][{e.Source}][{e.Message}]");
+            
         }
     }
 }
