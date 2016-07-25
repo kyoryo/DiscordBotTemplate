62a5025dc4de0e7c3d56876f959bbd869de6547f
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-07-25 16:04:48 +0700

    bot init

---
+++ DiscordBot00/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+        private DiscordClient _client;
+        public void Start()
+        {
+            _client = new DiscordClient();
+        }
+    }
+}
